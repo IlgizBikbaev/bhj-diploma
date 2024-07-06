a3f37d312bd7ad3ad05abaf0a95deb716fafad08
IlgizBikbaev <gizbikbaev@gmail.com>
2024-07-06 22:46:29 +0300

    add createRequest 1

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,29 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
+const createRequest = (options = {url, data, method, callback}) => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
 
+    if (options.method == "GET") {
+        xhr.open('GET', 'options.url?mail=options.data.mail&password=options.data.password');
+        xhr.send();
+    } else {
+        formData = new FormData;
+        formData.append('mail', 'options.data.mail');
+        formData.append('password', 'options.data.password');
+
+        xhr.open('POST', 'options.url');
+        xhr.send(formData);
+    }
+
+    xhr.addEventListener('readystatechange', () => {
+    try {
+            if (xhr.readyState == xhr.DONE && xhr.status === 200) {
+                options.callback(xhr.response.error, xhr.response);
+            }
+        } catch (error) {
+        options.callback(xhr.response.error);
+    }
+})
 };
